b0dcca3bafb44ed8180c8193dccae46978a637fa
KaxiYuan8288 <zihan.yuan@mail.utoronto.ca>
2023-07-18 12:58:05 -0400

    Updated User Interface, Attorney class, and Client class

--- src/main/java/userentities/Attorney.java
+++ src/main/java/userentities/Attorney.java
@@ -1,4 +1,20 @@
 package userentities;
 
 public class Attorney implements User{
+
+    private final int userID;
+    private final String password;
+
+    public Attorney(int userID, String password) {
+        this.userID = userID;
+        this.password = password;
+    }
+    @Override
+    public int getUserID() {
+        return userID;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 }
--- src/main/java/userentities/Client.java
+++ src/main/java/userentities/Client.java
@@ -1,4 +1,69 @@
 package userentities;
 
-public class Client {
+public class Client implements User{
+    private final int userID;
+    private final String password;
+    private final String stateAbb;
+    private final String postalCode;
+    private final String ethnicity;
+    private final int age;
+    private final String gender;
+    private final String maritalStatus;
+    private final int numberOfHousehold;
+    private final float annualIncome;
+
+    Client(int userID, String password, String stateAbb, String postalCode,
+           String ethnicity, int age, String gender, String maritalStatus,
+           int numberOfHousehold, float annualIncome){
+        this.userID = userID;
+        this.password = password;
+        this.stateAbb = stateAbb;
+        this.postalCode = postalCode;
+        this.ethnicity = ethnicity;
+        this.age = age;
+        this.gender = gender;
+        this.maritalStatus = maritalStatus;
+        this.numberOfHousehold = numberOfHousehold;
+        this.annualIncome = annualIncome;
+    }
+    @Override
+    public int getUserID() {
+        return userID;
+    }
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public String getStateAbb() {
+        return stateAbb;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getEthnicity() {
+        return ethnicity;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public int getNumberOfHousehold() {
+        return numberOfHousehold;
+    }
+
+    public float getAnnualIncome() {
+        return annualIncome;
+    }
 }
