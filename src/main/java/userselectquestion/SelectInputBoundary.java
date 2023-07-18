8a2180c97e6879c2d5fd9110659557414da8b726
Yifan Liu <yifanliu.liu@mail.utoronto.ca>
2023-07-18 19:44:29 -0400

    save partial works for select question usecase

--- src/main/java/userselectquestion/SelectInputBoundary.java
+++ src/main/java/userselectquestion/SelectInputBoundary.java
@@ -1,4 +1,5 @@
 package userselectquestion;
 
 public interface SelectInputBoundary {
+    TheQuestionResponseModel selectQuestion(SelectRequestModel selectRequestModel);
 }
--- src/main/java/userselectquestion/SelectQuestionInteractor.java
+++ src/main/java/userselectquestion/SelectQuestionInteractor.java
@@ -1,4 +1,20 @@
 package userselectquestion;
 
-public class SelectQuestionInteractor {
+import questionentities.Question;
+import questiongateway.QuestionGateway;
+import screenpresenter.ScreenOutputBoundary;
+
+public class SelectQuestionInteractor implements SelectInputBoundary{
+    final QuestionGateway questionGateway;
+    final ScreenOutputBoundary screenOutputBoundary;
+
+    public SelectQuestionInteractor(QuestionGateway questionGateway, ScreenOutputBoundary screenOutputBoundary) {
+        this.questionGateway = questionGateway;
+        this.screenOutputBoundary = screenOutputBoundary;
+    }
+
+    @Override
+    public TheQuestionResponseModel selectQuestion(SelectRequestModel selectRequestModel) {
+        return null;
+    }
 }
