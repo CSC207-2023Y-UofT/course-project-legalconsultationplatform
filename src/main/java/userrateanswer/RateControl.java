8d45363e76c24fb6bccd59d6aea71f28a37bd07d
Johnny <zhouyi.yu@mail.utoronto.ca>
2023-07-19 14:06:25 -0400

    Created RateControl, RateINputBoundary, RateRequestModel, RateInteractor

---
+++ src/main/java/userrateanswer/RateControl.java
@@ -0,0 +1,18 @@
+package userrateanswer;
+
+import presenter.MessageResponseModel;
+import questionentities.Question;
+
+public class RateControl {
+    private final RateInputBoundary rateInput;
+
+    public RateControl(RateInputBoundary input) {
+        this.rateInput = input;
+    }
+
+    MessageResponseModel rateAnswer(int rate, int questionId) {
+        RateRequestModel rating = new RateRequestModel(rate, questionId);
+        return rateInput.rateAnswer(rating);
+    }
+
+}
---
+++ src/main/java/userrateanswer/RateRequestModel.java
@@ -0,0 +1,23 @@
+package userrateanswer;
+
+import questionentities.Question;
+
+public class RateRequestModel {
+
+    private final int rating;
+    private final int answerId;
+
+    public RateRequestModel(int rating, int answerId) {
+        this.rating = rating;
+        this.answerId = answerId;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public int getAnswerId() {
+        return answerId;
+    }
+
+}
