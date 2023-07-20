f3d131295ce6e6969a45269713f15f209d1e1a08
josephpcsywm <josephpc0612@gmail.com>
2023-07-20 18:42:41 -0400

    commit add constructor

--- src/main/java/replytoquestion/ReplyInteractor.java
+++ src/main/java/replytoquestion/ReplyInteractor.java
@@ -10,8 +10,14 @@ public class ReplyInteractor implements PostInputBoundary{
     final MessageOutputBoundary messageOutputBoundary;
     final PostFactory postFactory;
 
+    public ReplyInteractor(QuestionGateway questionGateway, MessageOutputBoundary messageOutputBoundary, PostFactory postFactory){
+        this.questionGateway = questionGateway;
+        this.messageOutputBoundary = messageOutputBoundary;
+        this.postFactory = postFactory;
+    }
     @Override
     public MessageResponseModel createPost(PostRequestModel postRequestModel) {
+        postRequestModel.getQuestionId();
         return null;
     }
 }
