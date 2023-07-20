977d3a814a88067524c433b93651e6d878dccfaf
josephpcsywm <josephpc0612@gmail.com>
2023-07-20 08:54:19 -0400

    commit first Post entity version

--- src/main/java/questionentities/Post.java
+++ src/main/java/questionentities/Post.java
@@ -1,4 +1,28 @@
 package questionentities;
 
+import java.time.LocalDate;
+
 public class Post {
+    private final int questionId;
+    private final LocalDate createAt;
+    private final String postText;
+
+
+    public Post(int questionId, LocalDate createAt, String postText){
+        this.questionId = questionId;
+        this.createAt = createAt;
+        this.postText = postText;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public LocalDate getCreateAt() {
+        return createAt;
+    }
+
+    public String getPostText() {
+        return postText;
+    }
 }
