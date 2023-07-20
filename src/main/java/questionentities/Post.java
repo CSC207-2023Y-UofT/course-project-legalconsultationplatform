de5a541f62dd64e061f8820a93c277d8d1922d51
josephpcsywm <josephpc0612@gmail.com>
2023-07-20 18:44:30 -0400

    commit modify post entity

--- src/main/java/questionentities/Post.java
+++ src/main/java/questionentities/Post.java
@@ -4,18 +4,24 @@ import java.time.LocalDate;
 
 public class Post {
     private final int questionId;
+    private final int postId;
     private final LocalDate createAt;
     private final String postText;
 
 
-    public Post(int questionId, LocalDate createAt, String postText){
+    public Post(int questionId, int postId, LocalDate createAt, String postText){
         this.questionId = questionId;
+        this.postId = postId;
         this.createAt = createAt;
         this.postText = postText;
     }
 
     public int getQuestionId() {
         return questionId;
+    }
+
+    public int getPostId() {
+        return postId;
     }
 
     public LocalDate getCreateAt() {
