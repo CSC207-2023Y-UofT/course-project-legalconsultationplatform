1cf1846650867f341de07da7d59c88287c9d0e99
josephpcsywm <josephpc0612@gmail.com>
2023-07-20 08:46:32 -0400

    commit half way done reply use case

---
+++ src/main/java/replytoquestion/PostControl.java
@@ -0,0 +1,9 @@
+package replytoquestion;
+
+public class PostControl {
+    final PostInputBoundary postInput;
+
+    public PostControl(PostInputBoundary postInput){
+        this.postInput = postInput;
+    }
+}
