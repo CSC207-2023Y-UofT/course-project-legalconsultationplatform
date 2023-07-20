a76725a13d4ce3dd8dc32b1d99a8fe840130d8ba
josephpcsywm <josephpc0612@gmail.com>
2023-07-20 15:45:18 -0400

    commit start the interactor

--- src/main/java/replytoquestion/PostFactory.java
+++ src/main/java/replytoquestion/PostFactory.java
@@ -1,5 +1,11 @@
 package replytoquestion;
 
-public class PostFactory {
+import questionentities.Post;
 
+import java.time.LocalDate;
+
+public class PostFactory {
+    public Post create(int questionId, LocalDate createAt, String postText){
+        return new Post(questionId, createAt, postText);
+    }
 }
--- src/main/java/replytoquestion/ReplyInteractor.java
+++ src/main/java/replytoquestion/ReplyInteractor.java
@@ -1,5 +1,17 @@
 package replytoquestion;
 
-public class ReplyInteractor {
+import presenter.MessageOutputBoundary;
+import presenter.MessageResponseModel;
+import questiongateway.QuestionGateway;
 
+public class ReplyInteractor implements PostInputBoundary{
+
+    final QuestionGateway questionGateway;
+    final MessageOutputBoundary messageOutputBoundary;
+    final PostFactory postFactory;
+
+    @Override
+    public MessageResponseModel createPost(PostRequestModel postRequestModel) {
+        return null;
+    }
 }
