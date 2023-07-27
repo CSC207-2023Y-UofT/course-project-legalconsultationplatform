278be1e87115695dc7936e8c6e7c3c138bb1a670
Xingfu Wu <xingfu.wu@mail.utoronto.ca>
2023-07-27 19:15:44 -0400

    finish test QuestionTest

--- src/test/java/entitytesting/QuestionTest.java
+++ src/test/java/entitytesting/QuestionTest.java
@@ -12,8 +12,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class QuestionTest {
 
-    @Test
-    public void testFirstConstructor() {
+    void testConstructor() {
         // expected values
         int expectedQuestionId = 1000000;
         LocalDate expectedCreateAt = LocalDate.now();
@@ -21,7 +20,7 @@ public class QuestionTest {
         int expectedAskedByClient = 2000000;
         LocalDate expectedLegalDeadline = LocalDate.now();
 
-        // first constructor
+        // constructor
         Question question = new Question(expectedQuestionId, expectedType, expectedCreateAt, expectedAskedByClient,
                 expectedLegalDeadline);
 
@@ -38,48 +37,5 @@ public class QuestionTest {
         assertFalse(question.isClose(), "isClose should be false initially.");
         assertEquals(Question.MISSING_RATING, question.getRating(), "Rating should be MISSING_RATING initially.");
         assertTrue(question.getPosts().isEmpty(), "Posts should be empty initially.");
-    }
-
-    @Test
-    void testSecondConstructor() {
-        // expected values
-        int expectedQuestionId = 1000000;
-        LocalDate expectedCreateAt = LocalDate.now();
-        String expectedType = "fraud";
-        int expectedAskedByClient = 2000000;
-        boolean expectedIsTaken = true;
-        int expectedTakenByAttorney = 3000000;
-        boolean expectedIsClose = true;
-        int expectedRating = 5;
-        LocalDate expectedLegalDeadline = LocalDate.now();
-
-        // expected values for post
-        int expectedPostId = 1;
-        LocalDate expectedPostCreateAt = LocalDate.now();
-        String expectedPostText = "This is a test post.";
-
-        // create a post object
-        Post expectedPost = new Post(expectedQuestionId, expectedPostId, expectedPostCreateAt, expectedPostText);
-
-        // create a list and add the post object to it
-        List<Post> expectedPosts = new ArrayList<>();
-        expectedPosts.add(expectedPost);
-
-        // second constructor
-        Question question = new Question(expectedQuestionId, expectedType, expectedCreateAt, expectedAskedByClient,
-                expectedIsTaken, expectedTakenByAttorney, expectedIsClose, expectedRating, expectedPosts, expectedLegalDeadline
-        );
-
-        // assertions
-        assertEquals(expectedQuestionId, question.getQuestionId(), "QuestionId is wrong.");
-        assertEquals(expectedType, question.getType(), "Question type is wrong.");
-        assertEquals(expectedCreateAt, question.getCreateAt(), "CreateAt is wrong.");
-        assertEquals(expectedAskedByClient, question.getAskedByClient(), "AskedByClient is wrong.");
-        assertEquals(expectedLegalDeadline, question.getLegalDeadline(), "LegalDeadline is wrong.");
-        assertEquals(expectedIsTaken, question.isTaken(), "isTaken is wrong.");
-        assertEquals(expectedTakenByAttorney, question.getTakenByAttorney(), "takenByAttorney is wrong.");
-        assertEquals(expectedIsClose, question.isClose(), "isClose is wrong.");
-        assertEquals(expectedRating, question.getRating(), "Rating is wrong.");
-        assertEquals(expectedPosts, question.getPosts(), "Posts are wrong.");
     }
 }
