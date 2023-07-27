64e95ae490a81abd0230655ed744474308be7137
Johnny <zhouyi.yu@mail.utoronto.ca>
2023-07-26 21:53:18 -0400

    Changed a few lines of code, implemented the methods existsById, isClient, getUser, updateQuestionList

--- src/main/java/gateway/ClientRepository.java
+++ src/main/java/gateway/ClientRepository.java
@@ -16,28 +16,59 @@ public class ClientRepository implements ClientGateway{
 
     @Override
     public boolean existsById(int userId) {
-        return false;
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        try {
+            User exists = entityManager.find(User.class, userId);
+            return (exists != null);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public boolean isClient(int userId) {
-        return false;
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        try {
+            User user = entityManager.find(User.class, userId);
+            return (user instanceof Client);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public User getUser(int userId) {
-        return null;
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        try {
+            return entityManager.find(User.class, userId);
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void updateQuestionList(int userId, Question question) {
-
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        User user = getUser(userId);
+        try {
+            transaction.begin();
+            user.addQuestion(question);
+            entityManager.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
     }
 
     @Override
     public void addUser(User user) {
-        EntityManagerFactory entityManagerFactory = DatabaseConnection.getEntityManagerFactory();
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             transaction.begin();
@@ -52,5 +83,12 @@ public class ClientRepository implements ClientGateway{
             entityManager.close();
         }
     }
+
+    public static void main(String[] args) {
+        ClientRepository cr = new ClientRepository();
+        Client c = new Client();
+        cr.addUser(c);
+    }
+
 }
 
