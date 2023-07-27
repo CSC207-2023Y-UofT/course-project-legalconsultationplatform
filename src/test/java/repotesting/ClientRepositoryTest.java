c1677d41ef543aba3ce4dcbe77c7df024b95ab72
josephpcsywm <josephpc0612@gmail.com>
2023-07-27 19:34:47 -0400

    commit final version of reply use case version2.0

--- src/test/java/repotesting/ClientRepositoryTest.java
+++ src/test/java/repotesting/ClientRepositoryTest.java
@@ -16,20 +16,42 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ClientRepositoryTest {
     @Test
-    public void testMethod() {
-        int expectedAttorneyId = 1000000;
-        String expectedPassword = "abcd";
-        String expectedStateAbb = "state1";
-        String expectedPostalCode = "ABCD";
-
-        List<Question> expectedQuestionsList = new ArrayList<Question>();
+    public void testAddUserAndGetUser() {
+        int expectedClientId = 1000000;
 
         Client client = new Client();
-
-        DatabaseConnection database = new DatabaseConnection();
+        client.setUserId(expectedClientId);
 
         ClientRepository clientRepository = new ClientRepository();
 
         clientRepository.addUser(client);
+        assertEquals(clientRepository.getUser(expectedClientId), client, "The two clients are not the same");
+
+    }
+
+    @Test
+    public void testUpdateQuestionList(){
+
+        Client client = new Client();
+
+        ClientRepository clientRepository = new ClientRepository();
+
+        clientRepository.addUser(client);
+
+        int expectedQuestionId = 1000000;
+        String expectedType = "test type";
+        LocalDate expectedCreateAt = LocalDate.now();
+        int expectedAskedByClient = 1111111;
+        LocalDate expectedLegalDeadline = LocalDate.now();
+
+        Question question = new Question(expectedQuestionId, expectedType, expectedCreateAt, expectedAskedByClient, expectedLegalDeadline);
+
+        List<Question> expectedQuestionsList = new ArrayList<Question>();
+
+
+
+
+
+
     }
 }
