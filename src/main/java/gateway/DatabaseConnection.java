c960a3a4df90730b3edbdf37363b84c7fc2e347c
Yifan Liu <yifanliu.liu@mail.utoronto.ca>
2023-07-26 23:59:00 -0400

    test the remote server mode

--- src/main/java/gateway/DatabaseConnection.java
+++ src/main/java/gateway/DatabaseConnection.java
@@ -5,7 +5,7 @@ import javax.persistence.Persistence;
 
 public class DatabaseConnection {
 
-    private static final String URL = "objectdb:127.0.0.1:6136/db/LegalConsultationDB.odb";
+    private static final String URL = "objectdb://localhost:6136/myapp.odb;admin";
     private static EntityManagerFactory entityManagerFactory;
 
     public static EntityManager getEntityManager() {
