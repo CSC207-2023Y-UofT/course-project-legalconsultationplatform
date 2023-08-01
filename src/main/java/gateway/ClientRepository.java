bc4f964e30f904a0092460ca5219966d056defba
Yifan Liu <yifanliu.liu@mail.utoronto.ca>
2023-08-01 19:00:04 -0400

    Revert "Delete ClientRepository.java"
    This reverts commit 9929c0840f310768a1f8dddcdac6b3854561a135.

---
+++ src/main/java/gateway/ClientRepository.java
@@ -0,0 +1,93 @@
+package gateway;
+
+import questionentities.Question;
+import userentities.Client;
+import userentities.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.xml.crypto.Data;
+import java.util.Date;
+
+// TODO: implement this class
+public class ClientRepository implements ClientGateway{
+
+    @Override
+    public boolean existsById(int userId) {
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        try {
+            User exists = entityManager.find(User.class, userId);
+            return (exists != null);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public boolean existsByName(String userName) {
+        return false;
+    }
+
+    @Override
+    public boolean isClient(int userId) {
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        try {
+            User user = entityManager.find(User.class, userId);
+            return (user instanceof Client);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public User getUser(int userId) {
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        try {
+            return entityManager.find(User.class, userId);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public void updateQuestionList(int userId, Question question) {
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        User user = getUser(userId);
+        try {
+            transaction.begin();
+            user.addQuestion(question);
+            entityManager.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    @Override
+    public void addUser(User user) {
+        EntityManager entityManager = DatabaseConnection.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+}
+
