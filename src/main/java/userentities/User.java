befa9711fb1cfb88055aa506eb5dcd75514c1e61
Yifan Liu <123603507+YifanLiu2@users.noreply.github.com>
2023-08-02 00:22:47 -0400

    Update User.java

--- src/main/java/userentities/User.java
+++ src/main/java/userentities/User.java
@@ -5,12 +5,10 @@ import questionentities.Question;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 
-@MappedSuperclass
 public interface User {
     int getUserId();
     String getPassword();
     String getEmail();
-    @OneToMany
     List<Question> getQuestionsList();
     void addQuestion(Question question);
     boolean isClient();
