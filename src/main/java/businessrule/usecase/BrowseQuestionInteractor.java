b6be07e08387828edf6f78f975fe7cb5e4152154
Yifan Liu <yifanliu.liu@mail.utoronto.ca>
2023-08-07 15:34:04 -0400

    change browse question use case to make it only return not taken and not close question

--- src/main/java/businessrule/usecase/BrowseQuestionInteractor.java
+++ src/main/java/businessrule/usecase/BrowseQuestionInteractor.java
@@ -7,7 +7,10 @@ import businessrule.gateway.QuestionGateway;
 import entity.Question;
 import entity.User;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BrowseQuestionInteractor extends ViewQuestionInteractorBase {
     final AttorneyGateway attorneyGateway;
@@ -19,7 +22,12 @@ public class BrowseQuestionInteractor extends ViewQuestionInteractorBase {
 
     @Override
     protected List<Question> fetchQuestions(ViewRequestModel viewRequestModel) {
-        return questionGateway.getNotTakenQuestion();
+        List<Question> notTakenList = questionGateway.getNotTakenQuestion();
+        List<Question> notCloseList = questionGateway.getNotClosedQuestion();
+        Set<Question> notTakenSet = new HashSet<>(notTakenList);
+        Set<Question> notCloseSet = new HashSet<>(notCloseList);
+        notCloseSet.retainAll(notTakenSet);
+        return new ArrayList<>(notCloseSet);
     }
 
     @Override
