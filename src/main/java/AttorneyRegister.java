e6d3c84a950f50d917821bad75b2778d1448790e
river3966 <river.qi@mail.utoronto.ca>
2023-08-11 10:07:56 -0400

    small

--- src/main/java/AttorneyRegister.java
+++ src/main/java/AttorneyRegister.java
@@ -1,10 +1,243 @@
 import driver.database.AttorneyRepository;
 import entity.Attorney;
 
+
 public class AttorneyRegister {
     public static void main(String[] args) {
         AttorneyRepository attorneyRepository = new AttorneyRepository();
-        Attorney attorney1 = new Attorney();
-        attorneyRepository.save(attorney1);
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+//        ------------------------------------------------
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+        AttorneyRepository attorneyRepository = new AttorneyRepository();
+        AttorneyFactory attorneyFactory = new AttorneyFactory();
+        Set<String> professionals = new HashSet<>();
+        professionals.add("Family and Children");
+        professionals.add("Housing and Homelessness");
+        professionals.add("Education");
+        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678", "12345678", "CA", "12345", professionals);
+        registrationData.setUserId(12345678);
+        Attorney cathy = attorneyFactory.createUser(registrationData);
+        attorneyRepository.save(cathy);
+
+
+
     }
 }
