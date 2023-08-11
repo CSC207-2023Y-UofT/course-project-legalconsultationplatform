6ebe86075af1f54e5c00ee8e2f976a5b6f7aae72
river3966 <river.qi@mail.utoronto.ca>
2023-08-11 11:30:47 -0400

    ok

--- src/main/java/AttorneyRegister.java
+++ src/main/java/AttorneyRegister.java
@@ -9,17 +9,17 @@ import java.util.Set;
 
 public class AttorneyRegister {
     public static void main(String[] args) {
-        AttorneyRepository attorneyRepository = new AttorneyRepository();
-        AttorneyFactory attorneyFactory = new AttorneyFactory();
-        Set<String> professionals = new HashSet<>();
-        professionals.add("Family and Children");
-        professionals.add("Housing and Homelessness");
-        professionals.add("Education");
-        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678",
-                "12345678", "CA", "12345", professionals);
-        registrationData.setUserId(12345678);
-        Attorney cathy = attorneyFactory.createUser(registrationData);
-        attorneyRepository.save(cathy);
+//        AttorneyRepository attorneyRepository = new AttorneyRepository();
+//        AttorneyFactory attorneyFactory = new AttorneyFactory();
+//        Set<String> professionals = new HashSet<>();
+//        professionals.add("Family and Children");
+//        professionals.add("Housing and Homelessness");
+//        professionals.add("Education");
+//        RegistrationData registrationData = new RegistrationData("Cathy", "zihan.yuan@mail.utoronto.ca", "12345678",
+//                "12345678", "CA", "12345", professionals);
+//        registrationData.setUserId(12345678);
+//        Attorney cathy = attorneyFactory.createUser(registrationData);
+//        attorneyRepository.save(cathy);
 
 //        ------------------------------------------------
         AttorneyRepository attorneyRepository2 = new AttorneyRepository();
