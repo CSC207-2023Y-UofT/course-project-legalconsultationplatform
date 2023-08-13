25573b72d11baae2fb8cca402e9fc8a9772c93a9
Yifan Liu <yifanliu.liu@mail.utoronto.ca>
2023-08-13 19:02:01 -0400

    small changes

--- src/main/java/businessrule/UIFactory.java
+++ src/main/java/businessrule/UIFactory.java
@@ -1,38 +1,35 @@
 package businessrule;
 
-//import driver.screen.AskQuestionUI;
-//import driver.screen.BaseUI;
+import driver.screen.BaseUI;
 
 public class UIFactory {
     public enum UIType {
+        WELCOME_UI,
+        REGISTER_UI,
+        LOGIN_UI,
+        HOME_PAGE,
         ASK_QUESTION_UI,
-        ATTORNEY_HOME_PAGE,
-        CLIENT_HOME_PAGE,
-        QUESTION_LIST_UI,
-        THE_QUESTION_CLOSE_UI,
-        THE_QUESTION_OPEN_UI,
-        THE_QUESTION_ATTORNEY_UI
     }
 
-//    public static BaseUI createUI(UIType uiType) {
-//        switch (uiType) {
-//            case ASK_QUESTION_UI:
-//                return new AskQuestionUI();
-//            case ATTORNEY_HOME_PAGE:
-//                return new AttorneyHomePage();
-//            case CLIENT_HOME_PAGE:
-//                return new ClientHomePage();
-//            case QUESTION_LIST_UI:
-//                return new QuestionListUI();
-//            case THE_QUESTION_CLOSE_UI:
-//                return new QuestionCloseUI();
-//            case THE_QUESTION_OPEN_UI:
-//                return new QuestionOpenUI();
-//            case THE_QUESTION_ATTORNEY_UI:
-//                return new QuestionAttorneyUI();
-//            default:
-//                throw new IllegalArgumentException("Invalid UI type provided.");
-//        }
-//    }
+    public static BaseUI createUI(UIType uiType) {
+        switch (uiType) {
+            case ASK_QUESTION_UI:
+                return new AskQuestionUI();
+            case ATTORNEY_HOME_PAGE:
+                return new AttorneyHomePage();
+            case CLIENT_HOME_PAGE:
+                return new ClientHomePage();
+            case QUESTION_LIST_UI:
+                return new QuestionListUI();
+            case THE_QUESTION_CLOSE_UI:
+                return new QuestionCloseUI();
+            case THE_QUESTION_OPEN_UI:
+                return new QuestionOpenUI();
+            case THE_QUESTION_ATTORNEY_UI:
+                return new QuestionAttorneyUI();
+            default:
+                throw new IllegalArgumentException("Invalid UI type provided.");
+        }
+    }
 
 }
